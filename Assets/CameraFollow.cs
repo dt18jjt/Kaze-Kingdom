d220aaa055610a567a673328223a788408120fc6
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2021-04-06 23:34:17 +0100

    cam shake

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -8,18 +8,41 @@ public class CameraFollow : MonoBehaviour
     public Transform Target, Player, Rotator;
     public Transform playerTransform;
     private Vector3 cameraOffset;
+    public Transform camTransform;
     [Range(0.01f, 1.0f)]
     public float smoothFactor = 0.5f;
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+    Vector3 originalPos;
     // Start is called before the first frame update
     void Start()
     {
         cameraOffset = transform.position - playerTransform.position;
+        camTransform = GetComponent<Transform>();
+        originalPos = camTransform.localPosition;
     }
-
+    private void OnEnable()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
         Rotator.position = Player.transform.position;
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
     }
     private void LateUpdate()
     {
