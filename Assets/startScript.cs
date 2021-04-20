8c416d26c32fa216ac3d966f9a5b96a77da73a24
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2021-04-20 18:23:35 +0100

    level start

--- Assets/startScript.cs
+++ Assets/startScript.cs
@@ -4,48 +4,69 @@ using UnityEngine;
 
 public class startScript : MonoBehaviour
 {
-    public bool play = false;
+    public bool play = false, keyPress = false;
     public Transform Rotator;
     public Camera startCam, PlayCam;
-    public GameObject fadeIn, fadeOut;
+    public GameObject fadeIn, fadeOut, startImage, startText;
     public AudioSource startAudio, playAudio;
 
     // Start is called before the first frame update
     void Start()
     {
+        startImage.SetActive(true);
         startCam.enabled = true;
         PlayCam.enabled = false;
-        //GetComponent<AudioSource>().enabled = false;
         fadeIn.SetActive(false);
         fadeOut.SetActive(false);
+        //get all audio sources to play seperately
         AudioSource[] audios = GetComponents<AudioSource>();
         startAudio = audios[0];
         playAudio = audios[1];
+        //blinking text at start
+        StartCoroutine(textBlink());
+
     }
     private void FixedUpdate()
     {
+        //start camera rotation
         if (!play)
             Rotator.Rotate(0, -1, 0);
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        //start the game on any key press
+        if (Input.anyKey && !keyPress)
+        {
             StartCoroutine(beginFade());
+            keyPress = true;
+        }
+            
     }
     IEnumerator beginFade()
     {
-        fadeIn.SetActive(true);
-        startAudio.Play();
+        fadeIn.SetActive(true); //start fade in
+        startAudio.Play(); // play start audio
         yield return new WaitForSeconds(1.0f);
-        fadeIn.SetActive(false);
-        fadeOut.SetActive(true);
-        startCam.enabled = false;
-        PlayCam.enabled = true;
-        yield return new WaitForSeconds(1.8f);
-        //GetComponent<AudioSource>().enabled = true;
-        playAudio.Play();
+        startImage.SetActive(false); //images at the start disabled
+        fadeIn.SetActive(false); 
+        fadeOut.SetActive(true); // start fade out
+        startCam.enabled = false; // disable starting camera
+        PlayCam.enabled = true; // enable play camera
+        yield return new WaitForSeconds(1.9f);
+        playAudio.Play(); // play looping audio
         play = true;
         fadeOut.SetActive(false);
+    }
+    IEnumerator textBlink()
+    {
+        while (!keyPress)
+        {
+            startText.SetActive(true);
+            yield return new WaitForSeconds(.7f);
+            startText.SetActive(false);
+            yield return new WaitForSeconds(.7f);
+        }
+        
     }
 }
