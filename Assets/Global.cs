7f509878b92a1212aa6c7ca333311bf98b04c4b1
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2021-04-23 23:09:21 +0100

    level changes

--- Assets/Global.cs
+++ Assets/Global.cs
@@ -6,36 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class Global : MonoBehaviour
 {
-    public float timer, comboTime, comboTimeAmt = 5;
+    public float timer, comboTime, comboTimeAmt = 5, seconds, minutes;
     public int comboNum;
     public bool comboOn = true, paused;
     public Text timeText, comboText;
-    public Image fillImg;
+    public Image cmbfillImg, timfillImg;
     startScript start;
     // Start is called before the first frame update
     void Start()
     {
        comboTime = 0;
-       fillImg.fillAmount = comboTime;
+       cmbfillImg.fillAmount = comboTime;
+       timfillImg.fillAmount = 0;
        comboText.enabled = false;
        start = GetComponent<startScript>();
 
     }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
 
+        minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        //timeText.text = "Time Left: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeText.text = (minutes >= 1) ? minutes.ToString() : seconds.ToString();
+        timeText.color = (minutes >= 1) ? Color.white : Color.red;
+    }
     // Update is called once per frame
     void Update()
     {
+        //Timer countdown
         if (start.playing && !paused)
         {
             timer -= (timer > 0) ? Time.deltaTime : 0;
-            DisplayTime(timer);      
+            DisplayTime(timer);
+            timfillImg.fillAmount = (minutes >= 1) ? seconds / 60: 0;
         }
+        //Combo meter
         if (comboOn && comboTime > 0 && !paused)
         {
             comboText.enabled = true;          
             comboText.text = comboNum.ToString();
             comboTime -= Time.deltaTime;
-            fillImg.fillAmount = comboTime / comboTimeAmt;
+            cmbfillImg.fillAmount = comboTime / comboTimeAmt;
             if(comboTime <= 0)
             {
                 comboOn = false;
@@ -44,7 +58,8 @@ public class Global : MonoBehaviour
 
             }
         }
-        Time.timeScale = (paused) ? 0.1f : 1f;
+        Time.timeScale = (paused) ? 0f : 1f;
+        //Pausing
         if(Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.Joystick1Button7))
         {
             if (!paused && start.playing)
@@ -53,21 +68,15 @@ public class Global : MonoBehaviour
                 SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
             }
             else
+            {
                 paused = false;
+                SceneManager.UnloadSceneAsync("Pause");
+            }
+                
                 
         }
 
     }
-    void DisplayTime(float timeToDisplay)
-    {
-        timeToDisplay += 1;
-
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-
-        //timeText.text = "Time Left: " + string.Format("{0:00}:{1:00}", minutes, seconds);
-        timeText.text = (minutes >= 1) ? minutes.ToString() : seconds.ToString();
-        timeText.color = (minutes >= 1) ? Color.white : Color.red;
-    }
+   
 
 }
