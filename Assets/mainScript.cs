ce773a28674b53741d923c9f8f974d4a86bc2100
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2021-04-25 19:55:17 +0100

    tutorial finished

--- Assets/mainScript.cs
+++ Assets/mainScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class mainScript : MonoBehaviour
 {
     startScript start;
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/tutorialScript.cs
+++ Assets/tutorialScript.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class tutorialScript : MonoBehaviour
 {
     public int tNum = 0;
+    public GameObject next, back, play, mov, cam, obj, prog, dstr;
     startScript start;
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,59 @@ public class tutorialScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        switch (tNum)
+        {
+            case 0:
+                next.SetActive(true);
+                back.SetActive(false);
+                play.SetActive(false);
+                mov.SetActive(true);
+                cam.SetActive(false);
+                obj.SetActive(false);
+                prog.SetActive(false);
+                dstr.SetActive(false);
+                break;
+            case 1:
+                next.SetActive(true);
+                back.SetActive(true);
+                play.SetActive(false);
+                mov.SetActive(false);
+                cam.SetActive(true);
+                obj.SetActive(false);
+                prog.SetActive(false);
+                dstr.SetActive(false);
+                break;
+            case 2:
+                next.SetActive(true);
+                play.SetActive(false);
+                back.SetActive(true);
+                mov.SetActive(false);
+                cam.SetActive(false);
+                obj.SetActive(true);
+                prog.SetActive(false);
+                dstr.SetActive(false);
+                break;
+            case 3:
+                next.SetActive(true);
+                back.SetActive(true);
+                play.SetActive(false);
+                mov.SetActive(false);
+                cam.SetActive(false);
+                obj.SetActive(false);
+                prog.SetActive(true);
+                dstr.SetActive(false);
+                break;
+            case 4:
+                next.SetActive(false);
+                back.SetActive(true);
+                play.SetActive(true);
+                mov.SetActive(false);
+                cam.SetActive(false);
+                obj.SetActive(false);
+                prog.SetActive(false);
+                dstr.SetActive(true);
+                break;
+        }
     }
     public void Skip()
     {
